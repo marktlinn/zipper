6823add011e49f91d6a0252c708fc5cf3ae1de0b
marktlinn <marktlinn@gmail.com>
2024-09-14 17:40:12 +0100

    add: test data and compression and decompression working

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,63 @@
 package main
 
-import "log/slog"
+import (
+	"flag"
+	"log"
+	"log/slog"
+	"os"
+
+	"github.com/marktlinn/zipper/zip"
+)
 
 func main() {
-	slog.Info("Hello world!")
+	// Define the flags
+	filePath := flag.String("f", "", "Path to the input file")
+	outputPath := flag.String("o", "", "Path to the output file")
+	compress := flag.Bool("c", false, "Performs a compression operation")
+	decompress := flag.Bool("d", false, "Performs a decompression operation")
+
+	// Parse the flags
+	flag.Parse()
+
+	noOperation := !*compress && !*decompress
+	if *filePath == "" || noOperation || *outputPath == "" {
+		log.Fatalf("Please provide valid file path, operation, and output path.")
+	}
+
+	// Open the input file
+	inputFile, err := os.Open(*filePath)
+	if err != nil {
+		log.Fatalf("Failed to open file: %v", err)
+	}
+	defer inputFile.Close()
+
+	// Create the output file
+	outFile, err := os.Create(*outputPath)
+	if err != nil {
+		log.Fatalf("Failed to create output file: %v", err)
+	}
+	defer outFile.Close()
+
+	// Perform the operation
+
+	if *compress {
+		err = zip.Compress(inputFile, outFile, *filePath)
+		if err != nil {
+			slog.Error("Error compressing file", "error", err)
+			log.Fatalf("Failed to compress file: %v", err)
+		}
+		slog.Info("Compression successful")
+
+		return
+	} else if *decompress {
+		err = zip.Decompress(inputFile, outFile)
+		if err != nil {
+			slog.Error("Error decompressing file", "error", err)
+			log.Fatalf("Failed to decompress file: %v", err)
+		}
+		slog.Info("Decompression successful")
+
+		return
+	}
+	log.Fatal("Invalid operation: Use '-c' flag for compress or '-d' for decompress.")
 }
--- zip/extract.go
+++ zip/extract.go
@@ -1,1 +1,83 @@
 package zip
+
+import (
+	"archive/zip"
+	"bytes"
+	"fmt"
+	"io"
+	"log/slog"
+)
+
+const chunkSize = 2 * 1024 * 1024 // 2MB
+
+// Decompress processes a ZIP file from a reader and writes the contents to the writer.
+func Decompress(r io.Reader, w io.Writer) error {
+	for {
+		buf, n, err := readChunk(r)
+		if err != nil {
+			if err != io.EOF {
+				slog.Error("error reading from input", "error", err)
+				return fmt.Errorf("failed to read from input: %w", err)
+			}
+			if n == 0 { // End of file and no data read
+				break
+			}
+		}
+
+		if err := processChunk(buf[:n], w); err != nil {
+			return err
+		}
+
+		if err == io.EOF && n == 0 { // End of file with no more data to process
+			break
+		}
+	}
+
+	return nil
+}
+
+// readChunk reads a chunk of data from the reader.
+func readChunk(r io.Reader) ([]byte, int, error) {
+	buf := make([]byte, chunkSize)
+	n, err := r.Read(buf)
+	if err != nil && err != io.EOF {
+		slog.Error("error reading from input", "error", err)
+		return nil, 0, err
+	}
+	return buf, n, err
+}
+
+// processChunk processes a chunk of data and writes its contents to the writer.
+func processChunk(chunk []byte, w io.Writer) error {
+	zipReader, err := zip.NewReader(bytes.NewReader(chunk), int64(len(chunk)))
+	if err != nil {
+		slog.Error("error creating zip reader", "error", err)
+		return fmt.Errorf("failed to create zip reader: %w", err)
+	}
+
+	for _, f := range zipReader.File {
+		if err := processFile(f, w); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// processFile processes a single file from the ZIP archive.
+func processFile(f *zip.File, w io.Writer) error {
+	file, err := f.Open()
+	fmt.Printf("file name: %s\n", f.Name)
+	if err != nil {
+		slog.Error("error opening file in zip archive", "error", err)
+		return fmt.Errorf("failed to open file in zip archive: %w", err)
+	}
+	defer file.Close()
+
+	if _, err := io.Copy(w, file); err != nil {
+		slog.Error("error writing decompressed data", "error", err)
+		return fmt.Errorf("failed to write decompressed data: %w", err)
+	}
+
+	return nil
+}
